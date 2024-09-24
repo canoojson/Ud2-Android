4e031b0770a6e14cb289d95bf3b07b9bc3edcd50
Sergio Cano <Sergio.Cano.Pomer.2004@gmail.com>
2024-09-24 09:59:24 +0200

    Avances clase 24/09

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,19 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import com.example.myapplication.ui.theme.Pink40
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    EjemploTexto(
+                    EjemploImagen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -52,6 +60,31 @@ fun EjemploTexto(modifier: Modifier = Modifier){
     )
 
 
+}
+
+@Composable
+fun EjemploTextFieldRelleno(modifier: Modifier = Modifier){
+    var texto by remember { mutableStateOf("Texto")}
+
+    TextField(
+        value = texto,
+        onValueChange = { texto = it},
+        label = {Text(stringResource(R.string.etiqueta))},
+        modifier = modifier
+    )
+}
+
+@Composable
+fun EjemploImagen(modifier: Modifier = Modifier){
+    val imagen = painterResource(id = R.drawable.androidblack)
+
+    Image(
+        painter = imagen,
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        alpha = 0.5F,
+        modifier = Modifier.fillMaxSize()
+    )
 }
 
 @Preview(showBackground = true)
